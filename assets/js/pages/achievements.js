b01d0cb07bf2c9d57cd044680586070d4f5fe7b2
MoxoPixel <hello@emils.graphics>
2025-07-06 16:23:29 +0200

    Refactored achievments

--- assets/js/pages/achievements.js
+++ assets/js/pages/achievements.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 achievements {
                     id
                     name
+                    imageLink
                     description
                     rarity
                     hidden
@@ -39,22 +40,9 @@ document.addEventListener('DOMContentLoaded', () => {
                         .then(response => response.json())
                         .then(jsonData => {
                             const achievementsHTML = data.data.achievements.map(achievement => {
-                                let imageUrl = '';
-                                if (achievement.id === '676091c0f457869a94017a23') {
-                                    imageUrl = '675864eb86778bc82004127c.png';
-                                } else if (achievement.id === '676094451fec2f7426093be6') {
-                                    imageUrl = '675864efbe287595450c7686.png';
-                                } else {
-                                    const achievementData = jsonData.find(item => item.id === achievement.id);
-                                    imageUrl = achievementData ? achievementData.imageUrl : '';
-                                }
-                                const imgSrc = imageUrl 
-                                ? `${window.location.origin}/tarkynator/assets/img/achievements/${imageUrl.split('/').pop()}`
-                                : `${window.location.origin}/tarkynator/assets/img/achievements/Standard_14.png`;
-
                                 return `
                                     <div class="achievements-item card ${achievement.hidden ? 'ach-hidden' : ''} ${achievement.rarity.toLowerCase()}">
-                                        <img src="${imgSrc}" alt="${achievement.name}">
+                                        <img src="${achievement.imageLink}" alt="${achievement.name}">
                                         <div class="content">
                                             <h4>${achievement.name}</h4>
                                             <p>${achievement.description}</p>
